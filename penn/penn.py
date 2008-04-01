efab30d822b1b93a610f8011417b9452a7b82709
Daniel Tse <daniel@semanaut.com>
2008-04-01 12:19:48 +0000

    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/munge@296 d32a69a3-4c7f-4454-b852-5abc192257b5

---
+++ penn/penn.py
@@ -0,0 +1,23 @@
+from ..lex.lex import preserving_split
+
+class Node:
+    def __init__(self, tag, kids):
+        self.tag = tag
+        self.kids = kids
+
+    def __repr__(self):
+        return "<%s %s>" % (self.tag, ' '.join(repr(kid) for kid in self.kids))
+
+    def __iter__(self):
+        for kid in self.kids: yield kid
+
+class Leaf:
+    def __init__(self, tag, lex):
+        self.tag = tag
+        self.lex = lex
+
+    def __repr__(self):
+        return "<%s %s>" % (self.tag, self.lex)
+
+    def __iter__(self): yield self
+
---
+++ penn/tests.py
@@ -0,0 +1,17 @@
+import unittest
+from penn import Node, Leaf
+
+class TestPenn(unittest.TestCase):
+    def setUp(self):
+        self.n = Node('a', [Leaf('L', lex) for lex in ('b', 'c', 'd', 'e')])
+
+    def testNodeChildIteration(self):
+        kids = []
+        for kid in self.n: kids.append(kid)
+        self.assertEqual([kid.lex for kid in kids], ['b', 'c', 'd', 'e'])
+
+    def testNodeRepresentation(self):
+        self.assertEqual(repr(self.n), "<a <L b> <L c> <L d> <L e>>")
+
+if __name__ == '__main__':
+    unittest.main()
