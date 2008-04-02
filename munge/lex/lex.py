fe024e1b8e6f8f680a1155d1f4ec603cd898d230
Daniel Tse <daniel@semanaut.com>
2008-04-02 09:11:43 +0000

    changed preserving_split to a list view, so we can peek at the first token
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@310 d32a69a3-4c7f-4454-b852-5abc192257b5

--- munge/lex/lex.py
+++ munge/lex/lex.py
@@ -1,13 +1,16 @@
 def preserving_split(str, split_chars, skip_chars=" \t\r\n"):
-    cur = []
-    for char in str:
-        if char in split_chars or char in skip_chars:
-            if cur: 
-                yield ''.join(cur)
-                del cur[:]
-            if char in split_chars: yield char
-        else:
-            cur.append(char)
+    def _preserving_split():
+        cur = []
+        for char in str:
+            if char in split_chars or char in skip_chars:
+                if cur: 
+                    yield ''.join(cur)
+                    del cur[:]
+                if char in split_chars: yield char
+            else:
+                cur.append(char)
 
-    if cur: yield ''.join(cur)
+        if cur: yield ''.join(cur)
 
+    def append(l, e): l.append(e); return l
+    return reduce(append, _preserving_split(), [])
