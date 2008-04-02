b308f3cc1dccec008a7749ac7bc7aae493f8e717
Daniel Tse <daniel@semanaut.com>
2008-04-02 10:05:39 +0000

    rewrote preserving split as explicit iterator wrapping implicit iterator
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@311 d32a69a3-4c7f-4454-b852-5abc192257b5

--- munge/lex/lex.py
+++ munge/lex/lex.py
@@ -1,16 +1,39 @@
 def preserving_split(str, split_chars, skip_chars=" \t\r\n"):
-    def _preserving_split():
-        cur = []
-        for char in str:
-            if char in split_chars or char in skip_chars:
-                if cur: 
-                    yield ''.join(cur)
-                    del cur[:]
-                if char in split_chars: yield char
-            else:
-                cur.append(char)
+    class PressplitIterator:
+        def __init__(self, str, split_chars, skip_chars):
+            def _preserving_split():
+                cur = []
+                for char in str:
+                    if char in split_chars or char in skip_chars:
+                        if cur: 
+                            yield ''.join(cur)
+                            del cur[:]
+                        if char in split_chars: yield char
+                    else:
+                        cur.append(char)
 
-        if cur: yield ''.join(cur)
+                if cur: yield ''.join(cur)
 
-    def append(l, e): l.append(e); return l
-    return reduce(append, _preserving_split(), [])
+            self.generator = _preserving_split()
+
+            # generator may be empty and raise StopIteration
+            try:
+                self.top = self.generator.next()
+            except StopIteration:
+                self.top = None
+
+        def __iter__(self): return self
+
+        def peek(self): return self.top
+        def next(self):
+            if self.top is None: raise StopIteration
+
+            previous_top = self.top
+            try:
+                self.top = self.generator.next()
+            except StopIteration: 
+                self.top = None
+            return previous_top
+
+    return PressplitIterator(str, split_chars, skip_chars)
+
