90f1fa09c7527eee6e2fa88211ad0914b99fa3b8
Daniel Tse <daniel@semanaut.com>
2008-05-12 04:27:31 +0000

    Added 'short forms' definitions
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@401 d32a69a3-4c7f-4454-b852-5abc192257b5

---
+++ munge/cats/cat_defs.py
@@ -0,0 +1,16 @@
+# Defines 'short forms' and constants for a number of commonly used categories.
+
+from munge.cats.nodes import AtomicCategory
+from munge.cats.parse import parse_category
+
+S, N, NP, PP = (AtomicCategory(atom) for atom in "S N NP PP".split())
+LeftAbsorbedPunctuationCats = ", . `` : ; LRB RRB".split()
+RightAbsorbedPunctuationCats = ", . '' : ; LRB RRB".split()
+ConjPunctuationCats = ", ; :".split()
+SbNP, SfNP, NPbNP, NbN, SbNPbSbNP, \
+SbS, SfS, SbNPfSbNP, NP = [parse_category(cat) for cat in
+                        '''S\\NP S/NP NP\\NP N\\N (S\\NP)\\(S\\NP)
+                           S\\S  S/S (S\\NP)/(S\\NP) NP'''.split()]
+                  
+# Defines a short name for converting a category string to a category representation.         
+C = parse_category
