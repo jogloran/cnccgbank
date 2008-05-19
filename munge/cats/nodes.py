72c20aa95ffbd9d1874a863c51fc479860953dcf
Daniel Tse <daniel@semanaut.com>
2008-05-19 09:21:22 +0000

    rolled back category hashing implementation
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@431 d32a69a3-4c7f-4454-b852-5abc192257b5

--- munge/cats/nodes.py
+++ munge/cats/nodes.py
@@ -1,6 +1,5 @@
 from copy import copy
 from munge.util.exceptions import CatParseException
-from exthash import bernstein_hash
 import re
 
 BACKWARD, FORWARD = range(2)
@@ -11,23 +10,15 @@ class AtomicCategory(object):
     def __init__(self, cat, features=None):
         self.cat = cat
         self.features = features or []
-        self.hash = self.rehash()
 
     def feature_repr(self):
         '''Returns the concatenation of each feature in this category's feature set.'''
         return ''.join("[%s]" % feature for feature in self.features)
 
-    def __repr__(self, first=True, show_modes=True, show_features=True):
+    def __repr__(self, first=True, show_modes=True):
         '''A (non-evaluable) representation of this category. Ignores both its arguments
         so it can be treated uniformly with ComplexCategory.'''
-        return self.cat + (self.feature_repr() if show_features else "")
-        
-    def rehash(self):
-        self.hash = bernstein_hash(self.__repr__(show_modes=False, show_features=False))
-        return self.hash
-        
-    def __hash__(self):
-        return self.hash
+        return self.cat + self.feature_repr()
         
     @property
     def label(self): return None
@@ -54,7 +45,6 @@ class AtomicCategory(object):
         '''Determines if this category is equal to another, without inspecting any features.'''
 #        if self is other: return True
         if not other.is_leaf(): return False
-        if hash(self) != hash(other): return False
         return self.cat == other.cat
 
     def labelled(self, index=0): return index
@@ -92,72 +82,28 @@ class ComplexCategory(object):
             raise CatParseException('Invalid mode index %s.' % mode_index)
 
     def __init__(self, left, direction, right, mode=None, features=None, label=None):
-        self.left_, self.direction_, self.right_ = left, direction, right
-        self.mode_ = mode
-        self.features_ = features or []
+        self.left, self.direction, self.right = left, direction, right
+        self.mode = mode
+        self.features = features or []
         self.label = label
 
         self.slash = "\\" if self.direction == BACKWARD else "/"
-        
-        self.hash = self.rehash()
-        
-    def rehash(self):
-        self.hash = bernstein_hash(self.__repr__(show_modes=False, show_features=False))
-        return self.hash
-        
-    def __hash__(self):
-        return self.hash
-        
-    def get_left(self): return self.left_
-    def set_left(self, v):
-        self.left_ = v
-        self.rehash()
-        return self.left_
-    left = property(get_left, set_left)
-    
-    def get_right(self): return self.right_
-    def set_right(self, v):
-        self.right_ = v
-        self.rehash()
-        return self.right_
-    right = property(get_right, set_right)
-    
-    def get_direction(self): return self.direction_
-    def set_direction(self, v):
-        self.direction_ = v
-        self.rehash()
-        return self.direction_
-    direction = property(get_direction, set_direction)
-    
-    def get_mode(self): return self.mode_
-    def set_mode(self, v):
-        self.mode_ = v
-        self.rehash()
-        return self.mode_
-    mode = property(get_mode, set_mode)
-    
-    def get_features(self): return self.features_
-    def set_features(self, v):
-        self.features_ = v
-        self.rehash()
-        return self.features_
-    features = property(get_features, set_features)
 
     def feature_repr(self):
         '''Returns the concatenation of each feature in this category's feature set.'''
         return ''.join("[%s]" % feature for feature in self.features)
 
-    def __repr__(self, first=True, show_modes=True, show_features=True):
+    def __repr__(self, first=True, show_modes=True):
         '''A (non-evaluable) representation of this category.'''
         return "%(open)s%(lch)s%(slash)s%(mode)s%(rch)s%(close)s%(feats)s" % {
             'open': "" if first else "(",
-            'lch': self.left.__repr__(False, show_modes, show_features),
+            'lch': self.left.__repr__(False, show_modes),
             'slash': self.slash,
           #  'label': self.label if self.is_labelled() else "",
             'mode': ComplexCategory.get_mode_symbol(self.mode) if show_modes else "",
-            'rch': self.right.__repr__(False, show_modes, show_features),
+            'rch': self.right.__repr__(False, show_modes),
             'close': "" if first else ")",
-            'feats': self.feature_repr() if show_features else ""
+            'feats': self.feature_repr()
         }
 
     def clone(self):
@@ -187,15 +133,14 @@ class ComplexCategory(object):
         '''Determines if this category is equal to another, without inspecting any features.'''
 #        if self is other: return True
         if not other.is_compound(): return False
-        if hash(self) != hash(other): return False
+
         return (self.direction == other.direction and
                 self.left == other.left and
                 self.right == other.right)
 
     def labelled(self, index=0):
         '''Labels this category in-place. Labelling attaches indices to each slash of this
-category in a pre-order traversal of the category tree. This function returns the smallest
-unassigned index.'''
+category in a pre-order traversal of the category tree.'''
         self.label = index
         index += 1 # the current node gets the label _index_
         index = self.left.labelled(index)
