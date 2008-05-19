fb70df9c954d99a388423a6775cddf5634571268
Daniel Tse <daniel@semanaut.com>
2008-05-19 09:16:24 +0000

    tried to implement hashing to minimise deep comparisons. Performance is poor because I am using __repr__ as the value to hash. I will revert this in the following revision
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@430 d32a69a3-4c7f-4454-b852-5abc192257b5

--- lib/setup.py
+++ lib/setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup, Extension
 
 setup(name='pressplit', ext_modules=[ Extension('pressplit', sources=['pressplit.c']) ])
+setup(name='exthash', ext_modules=[ Extension('exthash', sources=['hash.c']) ])
 
