8a0add6ff87b3ac5bdb5f8a3aff999fd8ab15879
Daniel Tse <daniel@semanaut.com>
2008-05-24 03:11:40 +0000

    split tgrep node representations into their own file
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@454 d32a69a3-4c7f-4454-b852-5abc192257b5

--- munge/proc/tgrep/parse.py
+++ munge/proc/tgrep/parse.py
@@ -6,6 +6,7 @@ import sys, re
 import munge.ccg.nodes as ccg
 from munge.cats.cat_defs import C
 from munge.util.err_utils import warn, err
+from munge.proc.tgrep.nodes import *
 
 tokens = ("SLASH", "ATOM", "OP", "LPAREN", "RPAREN", "REGEX")
 
@@ -27,58 +28,6 @@ def t_error(t):
     
 def p_error(stk):
     err("Syntax error encountered.")
-
-class Node(object):
-    def __init__(self, anchor, constraints=None):
-        self.anchor = anchor
-        
-        if not constraints: constraints = []
-        self.constraints = constraints
-        
-    def __repr__(self):
-        return "%s%s%s" % (self.anchor, ' ' if self.constraints else '', ' '.join(str(c) for c in self.constraints))
-        
-    def is_satisfied_by(self, node):
-        if self.anchor.is_satisfied_by(node):
-            return all(constraint.is_satisfied_by(node) for constraint in self.constraints)
-        return False
-        
-class Constraint(object):
-    def __init__(self, operator, rhs):
-        self.operator = operator
-        self.rhs = rhs
-    def __repr__(self):
-        return "%s %s" % (self.operator, self.rhs)
-    def is_satisfied_by(self, node):
-        if self.operator == '<':
-            return self.rhs.is_satisfied_by(node.lch) or \
-                   self.rhs.is_satisfied_by(node.rch)
-        return False
-        
-class Group(object):
-    def __init__(self, node):
-        self.node = node
-    def __repr__(self):
-        return "{%s}" % self.node
-    def is_satisfied_by(self, node):
-        return self.node.is_satisfied_by(node)
-
-class Atom(object):
-    def __init__(self, value):
-        self.value = value
-    def __repr__(self):
-        return self.value
-    def is_satisfied_by(self, node):
-        return self.value == str(node.cat)
-        
-class RE(object):
-    def __init__(self, source):
-        self.source = source
-        self.regex = re.compile(source)
-    def __repr__(self):
-        return "/%s/" % self.source
-    def is_satisfied_by(self, node):
-        return self.regex.match(str(node.cat)) is not None
         
 def p_node(stk):
     '''
