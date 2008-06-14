6d129b4259ae51a767f52e6cbbad9ff25ee4e9d2
Daniel Tse <daniel@semanaut.com>
2008-06-14 02:31:34 +0000

    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@520 d32a69a3-4c7f-4454-b852-5abc192257b5

--- apps/make_surgery.py
+++ apps/make_surgery.py
@@ -7,9 +7,10 @@ import sys
 for bundle in DirFileGuessReader(sys.argv[1], verbose=False):
     tree = bundle.derivation
     text = tree.text()
+    sec, doc, deriv = bundle.spec_tuple()
 
     if text[-1] in ('``', '`'):
-        print line
+        print bundle.label()
         print "-1 d"
 
         print "%d:%d(%d)" % (sec, doc, deriv+1)
@@ -18,9 +19,9 @@ for bundle in DirFileGuessReader(sys.argv[1], verbose=False):
         elif text[-1] == "`":
             print "0 i=``|`"
             
-        debug("%s ends with start quote", line)
+        debug("%s ends with start quote", bundle.label())
     elif text[0] in ("''", "'"):
-        print line
+        print bundle.label()
         print "0 d"
 
         print "%d:%d(%d)" % (sec, doc, deriv-1)
@@ -29,5 +30,5 @@ for bundle in DirFileGuessReader(sys.argv[1], verbose=False):
         elif text[0] == "`":
             print "e i=''|'"
             
-        debug("%s begins with end quote", line)
+        debug("%s begins with end quote", bundle.label())
 
