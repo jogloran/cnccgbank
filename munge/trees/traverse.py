5713bae8374388c41444a3d91c1d70f06aff15cd
Daniel Tse <daniel@semanaut.com>
2008-06-18 14:00:02 +0000

    hacked treatment of erroneous tag in 21:61(24)
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@539 d32a69a3-4c7f-4454-b852-5abc192257b5

--- munge/trees/traverse.py
+++ munge/trees/traverse.py
@@ -48,10 +48,12 @@ def text(deriv, pred=lambda e: True):
     
 def is_ignored(node, ignoring_quotes=True):
     '''Is true for a PTB node which corresponds to no CCGbank node (traces or quote symbols).'''
+    # We check that the POS tag is not ':' to exclude the erroneous analysis in 21:61(24) 
+    # (see munge.proc.quote.quotify spans())
     return (re.match(r'-?NONE-?', node.tag) or
             (ignoring_quotes and
                ((re.match(r'^``?$', node.lex) or
-                (node.tag != "POS" and re.match(r"^''?$", node.lex))))))
+                (node.tag not in ("POS", ":") and re.match(r"^''?$", node.lex))))))
     
 def text_without_quotes_or_traces(deriv):
     '''Returns a list of the text under this node, ignoring quote symbols or traces.'''
