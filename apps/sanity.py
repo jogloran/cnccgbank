ae335c0e6cc7461d9bc253d60d76b1dec372cc70
Daniel Tse <daniel@semanaut.com>
2008-06-18 08:51:33 +0000

    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@532 d32a69a3-4c7f-4454-b852-5abc192257b5

---
+++ apps/sanity.py
@@ -0,0 +1,15 @@
+from munge.proc.filter import Filter
+from munge.util.dict_utils import CountDict
+
+class SanityChecks(Filter):
+    def __init__(self):
+        self.nderivs = 0
+        self.nwords = 0
+        
+    def accept_derivation(self, bundle):
+        self.nderivs += 1
+        self.nwords += len(bundle.text())
+        
+    def output(self):
+        print "nderivs: %d, nwords: %d" % (self.nderivs, self.nwords)
+        
