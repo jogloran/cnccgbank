d9f58f21e324a92af6a07334435443b526785e51
Daniel Tse <daniel@semanaut.com>
2008-10-26 06:53:57 +0000

    added commas_only script
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@597 d32a69a3-4c7f-4454-b852-5abc192257b5

---
+++ apps/commas_only.py
@@ -0,0 +1,25 @@
+from __future__ import with_statement
+from munge.proc.filter import Filter
+import os
+
+class CommasOnly(Filter):
+    def __init__(self, outdir):
+        Filter.__init__(self)
+        
+        self.outdir = outdir
+        
+#    puncts = ", : ; ? !".split()
+    puncts = (",",)
+    def accept_derivation(self, bundle):
+        if any(tok in self.puncts for tok in bundle.derivation.text()):
+            self.write_derivation(bundle)
+        
+    def write_derivation(self, bundle):
+        outpath = os.path.join(self.outdir, "%02d" % bundle.sec_no)
+        if not os.path.exists(outpath): os.makedirs(outpath)
+        
+        output_filename = os.path.join(outpath, "wsj_%02d%02d.auto" % (bundle.sec_no, bundle.doc_no))
+
+        with file(output_filename, 'a') as f:
+            print >>f, bundle
+        
