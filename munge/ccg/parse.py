a5d61d6923c87ee91b0e87bac4e5044413bb3c82
Daniel Tse <daniel@semanaut.com>
2008-11-29 03:01:36 +0000

    refactored node creation into factory
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@606 d32a69a3-4c7f-4454-b852-5abc192257b5

--- munge/ccg/parse.py
+++ munge/ccg/parse.py
@@ -4,54 +4,64 @@ from munge.util.exceptions import CCGbankParseException
 from munge.util.parse_utils import with_parens, shift_and_check, ensure_stream_exhausted
 from nodes import Node, Leaf
 
-def parse_tree(tree_string):
+class CCGNodeFactory(object):
+    node_class = Node
+    leaf_class = Leaf
+
+def parse_tree(tree_string, node_factory=CCGNodeFactory):
+    parser = CCGParser(node_factory)
+    
     toks = preserving_split(tree_string, "()<>", suppressors='<>')
 
-    deriv = read_paren(toks)
+    deriv = parser.read_paren(toks)
     ensure_stream_exhausted(toks, 'ccg.parse_tree')
 
     return deriv
+    
+class CCGParser(object):
+    def __init__(self, node_factory=CCGNodeFactory):
+        self.node_factory = node_factory
+        
+    def read_paren(self, toks, parent=None):
+        shift_and_check( '(', toks )
+        shift_and_check( '<', toks )
 
-def read_paren(toks, parent=None):
-    shift_and_check( '(', toks )
-    shift_and_check( '<', toks )
+        node_type = toks.peek()
+        if node_type == 'T':
+            result = self.read_internal_node(toks, parent)
+            shift_and_check( '>', toks )
 
-    node_type = toks.peek()
-    if node_type == 'T':
-        result = read_internal_node(toks, parent)
-        shift_and_check( '>', toks )
+            lch = self.read_paren(toks, result)
+            rch = None
+            if toks.peek() == '(': rch = self.read_paren(toks, result)
 
-        lch = read_paren(toks, result)
-        rch = None
-        if toks.peek() == '(': rch = read_paren(toks, result)
+            result.lch, result.rch = lch, rch
+        elif node_type == 'L':
+            result = self.read_leaf_node(toks, parent)
+            shift_and_check( '>', toks )
+        else:
+            raise CCGbankParseException, \
+                    "Node type (T, L) expected here."
 
-        result.lch, result.rch = lch, rch
-    elif node_type == 'L':
-        result = read_leaf_node(toks, parent)
-        shift_and_check( '>', toks )
-    else:
-        raise CCGbankParseException, \
-                "Node type (T, L) expected here."
+        shift_and_check( ')', toks )
 
-    shift_and_check( ')', toks )
+        return result
 
-    return result
+    def read_leaf_node(self, toks, parent=None):
+        shift_and_check( 'L', toks )
 
-def read_leaf_node(toks, parent=None):
-    shift_and_check( 'L', toks )
+        cat_string, pos1, pos2, lex, catfix = \
+                toks.next(), toks.next(), \
+                toks.next(), toks.next(), toks.next()
+        cat = parse_category(cat_string)
 
-    cat_string, pos1, pos2, lex, catfix = \
-            toks.next(), toks.next(), \
-            toks.next(), toks.next(), toks.next()
-    cat = parse_category(cat_string)
+        return self.node_factory.leaf_class(cat, pos1, pos2, lex, catfix, parent)
 
-    return Leaf(cat, pos1, pos2, lex, catfix, parent)
+    def read_internal_node(self, toks, parent):
+        shift_and_check( 'T', toks )
 
-def read_internal_node(toks, parent):
-    shift_and_check( 'T', toks )
+        cat_string, ind1, ind2 = \
+                toks.next(), toks.next(), toks.next()
+        cat = parse_category(cat_string)
 
-    cat_string, ind1, ind2 = \
-            toks.next(), toks.next(), toks.next()
-    cat = parse_category(cat_string)
-
-    return Node(cat, ind1, ind2, parent)
+        return self.node_factory.node_class(cat, ind1, ind2, parent)
