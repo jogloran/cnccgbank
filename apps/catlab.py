04e4c913979f5b73fddc0570d98005a6713e772d
Daniel Tse <daniel@semanaut.com>
2008-12-01 14:22:53 +0000

    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@608 d32a69a3-4c7f-4454-b852-5abc192257b5

--- apps/catlab.py
+++ apps/catlab.py
@@ -1,76 +1,194 @@
 from __future__ import with_statement
+from copy import copy
 from munge.proc.filter import Filter
 import os
 
+from echo import *
+from munge.trees.pprint import *
 from apps.identify_lrhca import *
 from munge.cats.nodes import *
 from munge.cats.cat_defs import *
 from munge.util.err_utils import info
 
+@echo
 def label_predication(node):
-    node.category = ptb_to_cat(node.tag)
-    label(node[0])
-    
-    node[1].category = node.category | node[0].category
-    
-    label(node[1])
-    
-def label_adjunction(node):
-    print "label_adj(%s)" % node
-    node[0].category = node.category / node.category
-    label(node[0])
+    if not node.category:
+        info('%s', node)
         
-    node[1].category = node.category
-    label(node[1])
+    node.kids[0] = label(node[0])
+    print "\t\t%s" % node.kids[0]
+    node[1].category = node.category | node[0].category
+    print "\t%s <- %s" % (node[1], node[1].category)
+    print "\t%s <- %s" % (node[0], node[0].category)
     
+    node.kids[1] = label(node[1])
+    
+    return node
+    
+@echo
+def label_left_absorption(node):
+    node[0].category = node[0].tag
+    node[1].category = node.category
+    node.kids[1] = label(node[1])
+    
+    return node
+    
+@echo
+def label_right_absorption(node):
+    node[1].category = node[1].tag
+    node[0].category = node.category
+    node.kids[0] = label(node[0])
+    
+    return node
+    
+@echo
+def label_adjunction(node):
+    info('%s', node)
+            
+    node[1].category = node.category
+    node[0].category = node.category / node.category
+    print "\t%s <- %s" % (node[1], node[1].category)
+    print "\t%s <- %s" % (node[0], node[0].category)
+    
+    node.kids[0] = label(node[0])
+    node.kids[1] = label(node[1])
+    
+    return node
+    
+@echo
+def label_right_adjunction(node):
+    info('%s', node)
+
+    node[0].category = node.category
+    node[1].category = node.category | node.category
+    print "\t%s <- %s" % (node[1], node[1].category)
+    print "\t%s <- %s" % (node[0], node[0].category)
+
+    node.kids[0] = label(node[0])
+    node.kids[1] = label(node[1])
+
+    return node
+    
+@echo
 def label_head_final(node):
-    node[0].category = label(node[0])
-    label(node[0])
+    info('%s', node)
+    
+    node.kids[0] = label(node[0])
     
     node[1].category = node.category | node[0].category
-    label(node[1])
+    print "\t%s <- %s" % (node[1], node[1].category)
+    print "\t%s <- %s" % (node[0], node[0].category)
     
+    node.kids[1] = label(node[1])
+    
+    return node
+    
+@echo
 def label_head_initial(node):
-    print "label_head_initial(%s)" % node
-    node[1].category = label(node[1])
-    label(node[1])
+    info('%s', node)
     
+    node.kids[1] = label(node[1])
     node[0].category = node.category / node[1].category
-    label(node[0])
+    print "\t%s <- %s" % (node[1], node[1].category)
+    print "\t%s <- %s" % (node[0], node[0].category)
     
+    node.kids[0] = label(node[0])
+    
+    return node
+    
+@echo
 def label_coordination(node):
-    pass
+#    node.category = ptb_to_cat(node.tag)
+    info('%s', node)
+
+    node.kids[0].category = node.category
+    print "\t%s <- %s" % (node[0], node[0].category)
     
+    node.kids[0] = label(node[0])
+
+    node.kids[1].category = node.category.clone().add_feature('conj')
+    node.kids[1] = label(node[1])
+    print "\t%s <- %s" % (node[1], node[1].category)
+    
+    return node
+    
+Map = {
+    'NP-SBJ': NP,
+    'NP-OBJ': NP,
+    'NP-PN-SBJ': NP,
+    'NP-PRD': NP,
+    'NP-EXT': NP,
+    'NP': NP,
+    'IP-HLN': S,
+    'IP': S,
+    'IP-SBJ': S,
+    'IP-OBJ': S,
+    'QP': C('NP/NP'),
+    'QP-OBJ': C('NP/NP'),
+    'CP': SbNP,
+    'DP': C('NP/NP'),
+    'LCP': AtomicCategory('LCP'),
+    'LCP-OBJ': AtomicCategory('LCP'),
+    'ADVP': SbNPfSbNP,
+    'VP': SbNP,
+    'PP-LOC': PP
+}
+@echo
 def ptb_to_cat(ptb_tag):
-#    print ptb_tag
-    return S
+    ptb_tag = base_tag(ptb_tag)
+    print ptb_tag
+    ptb_tag = Map.get(ptb_tag, AtomicCategory(ptb_tag))
+        
+    return copy(ptb_tag)
     
+@echo
 def label(node):
-    if node.is_leaf(): 
+    '''
+    Labels the descendants of _node_ and returns _node_.
+    '''
+    if (node.category is None):# and base_tag(node.tag) in Map:
         node.category = ptb_to_cat(node.tag)
+        
+    if node.is_leaf() or is_np_internal_structure(node): 
+        if not node.category:
+            node.category = ptb_to_cat(node.tag)
+        return node
     elif node.count() == 1: 
-        node.category = ptb_to_cat(node.tag)
-        label(node[0])
+        if not node.category:
+            node.category = ptb_to_cat(node.tag)
+        node.kids[0] = label(node[0])
+        return node
+        
+    elif is_left_absorption(node):
+        return label_left_absorption(node)
+    elif is_right_absorption(node):
+        return label_right_absorption(node)
         
     elif is_predication(node):
-        label_predication(node)
-    elif is_np_internal_structure(node):
-        label_adjunction(node)
-    elif is_adjunction(node):
-        label_adjunction(node)
-    elif is_head_final(node):
-        label_head_final(node)
-    elif is_head_initial(node):
-        label_head_initial(node)
+        return label_predication(node)
     elif is_coordination(node):
-        label_coordination(node)
+        return label_coordination(node)
+    elif is_np_internal_structure(node):
+        return label_adjunction(node)
+    elif is_right_adjunction(node): # (:h :a), for aspect particles
+        return label_right_adjunction(node)
+    elif is_adjunction(node):
+        return label_adjunction(node)
+    elif is_head_final(node):
+        return label_head_final(node)
+    elif is_head_initial(node):
+        return label_head_initial(node)
+    elif is_apposition(node):
+        return label_adjunction(node)
+
     else:
-        node.category = C('??')
-        for kid in node:
-            kid.category = C('??')
-            label(kid)
-            
+        return label_adjunction(node)
 #    print node
+
+def label_root(node):
+    node.category = ptb_to_cat(node.tag)
+    node = label(node)
+    return node
 
 class LabelNodes(Filter):
     def __init__(self, outdir):
@@ -78,7 +196,7 @@ class LabelNodes(Filter):
         self.outdir = outdir
         
     def accept_derivation(self, bundle):
-        label(bundle.derivation)
+        bundle.derivation = label_root(bundle.derivation)
         
         self.write_derivation(bundle)
         
--- apps/identify_lrhca.py
+++ apps/identify_lrhca.py
@@ -1,5 +1,23 @@
 import re
 
+from munge.proc.cn.count import last_nonpunct_kid
+
+def base_tag(tag):
+    if re.match(r'-.+-$', tag): return tag
+    
+    print "base_tag: %s ->" % tag,
+    tag = re.sub(r':.+$', '', tag)
+    tag = re.sub(r'-.+$', '', tag)
+    print tag
+    return tag
+
+def is_left_absorption(node):
+    return node[0].is_leaf() and node[0].tag == 'PU' and base_tag(node[1].tag) == base_tag(node.tag)
+    
+def is_right_absorption(node):
+    # TODO: refactor into one method
+    return node[1].is_leaf() and node[1].tag == 'PU' and base_tag(node[0].tag) == base_tag(node.tag)
+
 def is_np_sbj(node):
     return re.match(r'NP(-\w+)*-SBJ', node.tag) is not None
 
@@ -12,19 +30,25 @@ def is_predication(node):
             is_vp(node[1]))
 
 def is_head_final(node):
-    return node[-1].tag.endswith(':h')
+    return last_nonpunct_kid(node).tag.endswith(':h')
 
 def is_head_initial(node):
     return node[0].tag.endswith(':h')
 
 def is_adjunction(node):
     return node[0].tag.endswith(':a')
+    
+def is_right_adjunction(node):
+    return node[1].tag.endswith(':a')
 
 def is_coordination(node):
     return node[0].tag.endswith(':c')
+    
+def is_apposition(node):
+    return node[0].tag.endswith(':A')
 
 def is_np_internal_structure(node):
-    return node.tag.startswith('NP') and node.count() > 1 and all(kid.tag == 'NN' for kid in node)
+    return node.tag.startswith('NP') and node.count() > 1 and all(kid.tag in ('NN', 'PU') for kid in node)
     
 def is_apposition(node):
     return node[0].tag.endswith(':A')
