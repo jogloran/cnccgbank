44316d1d0b7ab344607195a14cd777268cef8140
Daniel Tse <daniel@semanaut.com>
2009-03-06 03:02:52 +0000

    adding make fix script
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@634 d32a69a3-4c7f-4454-b852-5abc192257b5

---
+++ make_fix.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+function do_fix {
+    fix_suffix=$1; shift
+    filter_name=$1; shift
+    paths=$@
+
+    rm -rf ./fixed_${fix_suffix}; ./t -lapps.cn.fix_${fix_suffix} -r $filter_name fixed_${fix_suffix} -0 -R AugmentedPTBReader $paths
+    ./t -q -w fixed_${fix_suffix}_dots -R AugmentedPTBReader fixed_${fix_suffix}/*
+}
+
+paths=$@
+do_fix rc FixExtraction $paths 2> rc_errors
+#do_fix prodrop FixProDrop fixed_rc/*
+#do_fix adverbs FixAdverbs fixed_prodrop/*
