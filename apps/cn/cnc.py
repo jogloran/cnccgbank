2ecf8a3205f23736d3d548d29051c4ee857e9877
Daniel Tse <daniel@semanaut.com>
2009-10-06 00:13:28 +0000

    added pipe style output
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@660 d32a69a3-4c7f-4454-b852-5abc192257b5

---
+++ apps/cn/cnc.py
@@ -0,0 +1,32 @@
+from munge.proc.filter import Filter
+from apps.cn.output import *
+from munge.trees.traverse import leaves
+
+class PipeFormat(Filter, OutputDerivation):
+    def __init__(self, outdir, format):
+        Filter.__init__(self)
+        OutputDerivation.__init__(self, transformer=self.transformer)
+        
+        self.outdir = outdir
+        self.format_string = self.make_format_string_from(format)
+        
+    def accept_derivation(self, bundle):
+        self.write_derivation(bundle)
+        
+    def transformer(self, bundle):
+        return (" ".join(self.format(leaf))
+                for leaf in leaves(bundle.derivation))
+    
+    @staticmethod
+    def make_format_string_from(format):
+        substitutions = {
+            "%f": "%(lex)s",
+            "%i": "%(pos)s",
+            "%c": "%(cat)s"
+        }
+        for src, dst in substitutions.iteritems():
+            format = re.sub(src, dst, format)
+        return format
+        
+    def format(self, leaf):
+        return self.format_string % {'lex': leaf.lex, 'pos': leaf.pos1, 'cat': str(leaf.cat)}
