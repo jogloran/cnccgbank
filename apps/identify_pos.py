71dcc28aeff17f6ccf5cbd94dded38ab35e444d2
Daniel Tse <daniel@semanaut.com>
2009-10-25 06:30:13 +0000

    Fixed broken build
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@683 d32a69a3-4c7f-4454-b852-5abc192257b5

--- apps/identify_pos.py
+++ apps/identify_pos.py
@@ -1,13 +1,13 @@
 from munge.trees.traverse import leaves
 
-#VerbalCategories = ('VV', 'VA', 'VC', 'VE')
+VerbalCategories = ('VV', 'VA', 'VC', 'VE')
     
 def has_verbal_tag(node):
     try:
         return node.tag[0] == 'V' and node.tag[1] in ('V', 'A', 'C', 'E')
     except: return False
     
-#NominalCategories = ('NN', 'NR', 'NT')
+NominalCategories = ('NN', 'NR', 'NT')
 
 def has_noun_tag(node):
     try:
