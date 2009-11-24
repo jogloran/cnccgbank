775a53596413c8bf0ea55f240624e18f6f4f6a09
Daniel Tse <daniel@semanaut.com>
2009-11-24 04:05:09 +0000

    fixed broken commit
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@762 d32a69a3-4c7f-4454-b852-5abc192257b5

--- apps/cmd.py
+++ apps/cmd.py
@@ -267,7 +267,7 @@ a pager program.'''
                make_option('-t', '--tokens', help='Print tokens of matching trees.',
                            dest='show_mode', action='store_const', const='tokens'),
                make_option('-T', '--tokens-only', help='Print only tokens of matching trees.',
-                           dest='show_mode', action='store_const', const='tokens_only')
+                           dest='show_mode', action='store_const', const='tokens_only'),
                make_option('-r', '--rule', help='Shows the local category context of the match.',
                            dest='show_mode', action='store_const', const='rule'),
                            
