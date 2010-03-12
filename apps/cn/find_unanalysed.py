62f1098174d612ca00c72205ad0f6a51779073d4
Daniel Tse <daniel@semanaut.com>
2010-03-12 11:52:12 +0000

    find_unanalysed now does stats for UCPs
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@992 d32a69a3-4c7f-4454-b852-5abc192257b5

--- apps/cn/find_unanalysed.py
+++ apps/cn/find_unanalysed.py
@@ -7,15 +7,24 @@ from collections import defaultdict
 from munge.util.err_utils import debug
 from apps.util.config import config
 from munge.util.dict_utils import sorted_by_value_desc
+from munge.cats.cat_defs import *
 
 config.show_vars = False
 
 combs = defaultdict(lambda: 0)
 unrecognised_rules = defaultdict(lambda: 0)
 total, with_unrecognised_rules = 0, 0
+ucp_rules = defaultdict(lambda: 0)
+with_ucp = 0
+
+def is_ucp(l, r, p):
+    if r is None: return False
+    
+    return l == conj and p.has_feature('conj') and p != r
+
 for file in glob(sys.argv[1]):
     for bundle in CCGbankReader(file):
-        has_unrecognised_rules = False
+        has_unrecognised_rules, has_ucp = False, False
         
         for node in nodes(bundle.derivation):
             if node.is_leaf(): continue
@@ -25,8 +34,13 @@ for file in glob(sys.argv[1]):
             comb = analyse(*lrp)
             if comb:
                 combs[comb] += 1
+            elif is_ucp(*lrp):
+                l, r, p = lrp
+                ucp_rules[ (str(l), str(r), str(p)) ] += 1
+                if not has_ucp:
+                    with_ucp += 1
+                has_ucp = True
             else:
-                #debug("Unrecognised rule %s %s -> %s", *lrp)
                 l, r, p = lrp
                 unrecognised_rules[ (str(l), str(r), str(p)) ] += 1
                 if not has_unrecognised_rules:
@@ -37,5 +51,10 @@ for file in glob(sys.argv[1]):
 
 print "With unrecognised rules: %d/%d=%.2f" % (with_unrecognised_rules, total, with_unrecognised_rules/float(total)*100.0)
 for rule, freq in sorted_by_value_desc(unrecognised_rules):
+    l, r, p = rule
+    print "% 4d | %20s %20s -> %s" % (freq, l, r, p)
+
+print "UCP rules: %d/%d=%.2f" % (with_ucp, total, with_ucp/float(total)*100.0)
+for rule, freq in sorted_by_value_desc(ucp_rules):
     l, r, p = rule
     print "% 4d | %20s %20s -> %s" % (freq, l, r, p)
