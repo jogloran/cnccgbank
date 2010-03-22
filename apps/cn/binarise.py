a2072f87b0324355fcf63f833bdea134e9368e25
Daniel Tse <daniel@semanaut.com>
2010-03-22 06:52:40 +0000

    reverted
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@1037 d32a69a3-4c7f-4454-b852-5abc192257b5

--- apps/cn/binarise.py
+++ apps/cn/binarise.py
@@ -40,14 +40,13 @@ def label_adjunction(node, inherit_tag=False, without_labelling=False, inside_np
     cur.tag = node.tag
     return cur
     
-def label_apposition(node, inherit_tag=False, inside_np_internal_structure=False):
+def label_apposition(node, inherit_tag=False):
     kid_tag = strip_tag_if(not inherit_tag, node.tag)
 
     if node.count() > 2:
         first, rest = node.kids.pop(0), node.kids
-        return Node(kid_tag, [label_node(first, inside_np_internal_structure=inside_np_internal_structure), 
-                              label_node(node, inside_np_internal_structure=inside_np_internal_structure)])
-    return label_adjunction(node, inherit_tag=inherit_tag, inside_np_internal_structure=inside_np_internal_structure)
+        return Node(kid_tag, [label_node(first), label_node(node)])
+    return label_adjunction(node, inherit_tag=inherit_tag)
     
 #@echo
 def label_np_internal_structure(node, inherit_tag=False):
@@ -329,7 +328,7 @@ def _label_node(node, inside_np_internal_structure=False, do_shrink=True):
         # although we want a head-initial analysis, we want a right-branching structure
         return label_adjunction(node, inside_np_internal_structure=True)
     elif is_apposition(node):
-        return label_apposition(node, inside_np_internal_structure=node.tag.startswith('NP'))
+        return label_apposition(node)
     elif is_np_structure(node):
         return label_adjunction(node, inside_np_internal_structure=True) # TODO: misnomer
     elif is_np_internal_structure(node):
