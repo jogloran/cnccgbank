9ae3f25b7e9d7540cf0eb259b772ac61ac307cad
Daniel Tse <daniel@semanaut.com>
2010-03-22 11:25:11 +0000

    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@1042 d32a69a3-4c7f-4454-b852-5abc192257b5

--- apps/cn/fix_rc.py
+++ apps/cn/fix_rc.py
@@ -168,7 +168,7 @@ class FixExtraction(Fix):
         # Relabel the relativiser category (NP/NP)\S to (NP/NP)\(S|NP)
         
 #        debug('Looking under %s', pprint(node))
-        result = get_first(node, r'*=S $ /DE[CG]/=REL', with_context=True, left_to_right=True)
+        result = get_first(node, r'*=S $ /DEC/=REL', with_context=True, left_to_right=True)
         # if result is None:
         #     # There's a mis-annotated DEG for DEC in 21:2(1) inter alia
         #     result = get_first(node, r'/DEG/=REL $ *=S', with_context=True, left_to_right=True)
