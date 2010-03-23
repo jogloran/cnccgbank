e7554ed57e14d286b259ac77a8f0d39d7e5a7944
Daniel Tse <daniel@semanaut.com>
2010-03-23 23:59:09 +0000

    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@1061 d32a69a3-4c7f-4454-b852-5abc192257b5

--- munge/cats/cat_defs.py
+++ munge/cats/cat_defs.py
@@ -18,8 +18,8 @@ RightAbsorbedPunctuationCats = ", . '' : ; LRB RRB".split()
 ConjPunctuationCats = ", ; :".split()
 
 if config.cn_puncts:
-    LeftAbsorbedPunctuationCats += "LCM LPA LQU RQU LSQ LTL RTL LCD LCS RCS DSH SLS ? !".split()
-    RightAbsorbedPunctuationCats += "LCM RPA LQU RQU RSQ RTL RCD RCS DSH SLS ? !".split()
+    LeftAbsorbedPunctuationCats += "LCM LPA LQU RQU LSQ LTL RTL LCD RCD LCS RCS DSH SLS ? !".split()
+    RightAbsorbedPunctuationCats += "LCM RPA LQU RQU RSQ LTL RTL LCD RCD LCS RCS DSH SLS ? !".split()
     ConjPunctuationCats.append("LCM")
 
 SbNP, SfNP, NPbNP, NPfNP, NbN, NfN, SbNPbSbNP, \
