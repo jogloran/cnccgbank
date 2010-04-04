f455e44f16b64d569aaf33cc897b793a6a02d380
Daniel Tse <daniel@semanaut.com>
2010-04-04 06:30:05 +0000

    cleanup
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@1177 d32a69a3-4c7f-4454-b852-5abc192257b5

--- apps/identify_pos.py
+++ apps/identify_pos.py
@@ -4,16 +4,15 @@ VerbalCategories = ('VV', 'VA', 'VC', 'VE')
     
 def has_verbal_tag(node):
     try:
-        return node.tag[0] == 'V' and node.tag[1] in ('V', 'A', 'C', 'E')
+        return node.tag[0] == 'V' and node.tag[1] in 'VACE'
     except: return False
     
 NominalCategories = ('NN', 'NR', 'NT')
 
 def has_noun_tag(node):
     try:
-        return node.tag[0] == 'N' and node.tag[1] in ('N', 'T', 'R')
+        return node.tag[0] == 'N' and node.tag[1] in 'NTR'
     except: return False
 
 def is_verb_compound(node):
-#    return all((has_verbal_tag(kid) or kid.tag == 'CC') for kid in leaves(node))
     return any(node.tag.startswith(cand) for cand in ('VPT', 'VSB', 'VRD', 'VCD', 'VNV'))
