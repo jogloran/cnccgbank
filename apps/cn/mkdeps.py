ac368e0552136c51c78d42e50a99c820acfdd077
Daniel Tse <daniel@semanaut.com>
2010-04-12 11:04:05 +0000

    split mkdeps related functions into mkdeps_utils
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@1199 d32a69a3-4c7f-4454-b852-5abc192257b5

--- apps/cn/mkdeps.py
+++ apps/cn/mkdeps.py
@@ -1,8 +1,7 @@
 # coding=utf-8
 from apps.util.config import config
 config.set(show_vars=True) # override show_vars. must come before cats.nodes import
-    
-from itertools import izip
+
 from copy import deepcopy
 
 from apps.cn.mkmarked import naive_label_derivation
@@ -16,38 +15,7 @@ from munge.util.err_utils import debug, warn
 from munge.trees.pprint import pprint
 
 from apps.cn.mkmarked import is_modifier
-
-def copy_vars(frm, to):
-    '''Given two categories _frm_ and _to_, assumed to be the same modulo slots, overwrites the
-slots of _to_ with those of _frm_.'''
-    for (frms, tos) in izip(frm.nested_compound_categories(), to.nested_compound_categories()):
-        tos.slot = frms.slot
-
-def update_with_fresh_var(node, replacement):
-    '''Where _node_ is a node, and C is its category, replaces all variables in sub-categories of C
-with the same name as C's outermost variable with the slot _replacement_.'''
-    # change all occurrences of vars with the same var name as c to the same fresh var
-    to_replace = node.cat.slot.var
-    for sub in node.cat.nested_compound_categories():
-        if sub.slot.var == to_replace:
-            sub.slot = replacement
-            
-def no_unassigned_variables(cat):
-    '''Yields whether any sub-category inside category _cat_ has a slot without a variable.'''
-    for subcat in cat.nested_compound_categories():
-        if subcat.slot.var == '?': return False
-    return True
-    
-fresh_var_id = 1
-def fresh_var(prefix='F'):
-    '''Returns a unique variable name with a given _prefix_.'''
-    global fresh_var_id
-    ret = prefix + str(fresh_var_id)
-    fresh_var_id += 1
-    return ret
-    
-def strip_index(s):
-    return s.split('*')[0]
+from apps.util.mkdeps_utils import *
 
 unanalysed = set()
 def mkdeps(root):
@@ -256,35 +224,7 @@ def mkdeps(root):
                 result.add( (strip_index(sdepl), strip_index(sdepr)) )
                 
     return result
-
-LEFT, RIGHT = 1, 2
-class UnificationException(Exception): pass
-def unify(L, R, ignore=False, copy_vars=True):
-    assgs = []
     
-    for (Ls, Rs) in izip(L.nested_compound_categories(), R.nested_compound_categories()):
-        if Ls.slot.is_filled() and Rs.slot.is_filled():
-            if (not ignore) and Ls.slot.head.lex != Rs.slot.head.lex:
-                raise UnificationException('%s and %s both filled' % (Ls, Rs))
-
-        elif Ls.slot.is_filled():
-            Rs.slot.head.lex = Ls.slot.head.lex
-            Rs.slot.head.filler = L
-            Rs.slot.head.slash = Ls.label
-            assgs.append( (Rs, Ls.slot.head.lex) )
-
-        elif Rs.slot.is_filled():
-            Ls.slot.head.lex = Rs.slot.head.lex
-            Ls.slot.head.filler = R
-            Ls.slot.head.slash = Rs.label
-            assgs.append( (Ls, Rs.slot.head.lex) )
-
-        else: # both slots are variables, need to unify variables
-            if copy_vars: Rs.slot.head = Ls.slot.head
-            assgs.append( (Rs, Ls) )
-            
-    return assgs
-
 class MakeDependencies(Filter):
     def __init__(self, outdir):
         Filter.__init__(self)
