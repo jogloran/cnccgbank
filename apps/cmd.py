c31fc7dd244b3ba1a0da3b640c395cac9a0b2d28
Daniel Tse <daniel@semanaut.com>
2010-04-20 02:49:22 +0000

    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@1214 d32a69a3-4c7f-4454-b852-5abc192257b5

--- apps/cmd.py
+++ apps/cmd.py
@@ -33,83 +33,83 @@ class Shell(HistorySavingDefaultShell):
     '''A shell interface to trace functionality.'''
     PagerFilename = 'pager'
     StdoutFilename = 'stdout'
-    
+
     def __init__(self, pager_path=None, files=None, verbose=True, clear_history=False):
         HistorySavingDefaultShell.__init__(self, clear_history=clear_history)
-        
+
         self.tracer = TraceCore(libraries=BuiltInPackages, verbose=verbose, reader_class_name=config.cmd_reader_class)
         self.prompt = "trace> "
-        
+
         self.files = files or []
-        
+
         self.last_exception = None
 
         self._verbose = verbose
-        
+
         if pager_path:
             self.pager_path = pager_path
             self.output_file = PagerFilename
         else:
             self.pager_path = self.output_file = None
-        
+
     def get_verbose(self): return self._verbose
     def set_verbose(self, is_verbose):
         self._verbose = self.tracer.verbose = is_verbose
     verbose = property(get_verbose, set_verbose)
-        
+
     def do_quiet(self, args):
-        if self._verbose: 
+        if self._verbose:
             self.set_verbose(False)
             msg("Will generate less output.")
-            
+
     def do_verbose(self, args):
         if not self._verbose:
             self.set_verbose(True)
             msg("Will generate more output.")
-        
+
     def preloop(self):
         '''Executed before the command loop is entered.'''
         DefaultShell.preloop(self)
         # remove '-' (for option completion) and a number of shell separators (for path completion)
         # from the set of completer delimiters
         readline.set_completer_delims(re.sub(r'[-/*~\\]', '', readline.get_completer_delims()))
-        
+
     def precmd(self, line):
         '''Executed before every command is interpreted.'''
         cleaned_line = line.strip()
-        
+
         self._hist.append(cleaned_line)
         return cleaned_line
-        
+
     def do_load(self, args):
         '''Loads filter modules given as package names.'''
         modules = args.split()
-        
+
         old_modules = set(self.tracer.available_filters_dict.keys())
         self.tracer.add_modules(modules)
         modules = set(self.tracer.available_filters_dict.keys())
-        
+
         added_modules = modules.difference(old_modules)
         if added_modules:
             msg("%s modules added:", len(added_modules))
-            for module in added_modules: 
+            for module in added_modules:
                 msg("\t%s", module)
         else:
             msg("No modules added.")
 
     @options([ make_option('-l', '--long', action='store_true', dest='long', help='Show detailed summary', default=False),
-               make_option('-s', '--sort-by', action='store', type='choice', choices=['name', 'module', 'opt', 'long-opt'], 
+               make_option('-s', '--sort-by', action='store', type='choice', choices=['name', 'module', 'opt', 'long-opt'],
                            dest='sort_key', help='Display filters in a given sorted order', default='name')])
     def do_list(self, args, opts):
         """Lists all loaded filters."""
         self.tracer.list_filters(long=opts.long, filter_sort_key=opts.sort_key)
-        
+
     def do_with(self, args):
         '''Changes or displays the working set.'''
         args = args.split()
         if args:
             self.files = list(flatten(glob.glob(arg) for arg in args))
-            
+
         msg("Working set is: " + list_preview(self.files))
 
     def get_filter_by_switch(self, switch_name):
@@ -126,11 +126,11 @@ class Shell(HistorySavingDefaultShell):
 
         err("No filter with switch %s found.", switch_name)
         return None
-        
+
     def do_run(self, args):
         '''Runs the given filter with the given arguments on the working set.'''
         args = args.split()
-        
+
         if not args: return
 
         filter_name = args.pop(0)
@@ -143,7 +143,7 @@ class Shell(HistorySavingDefaultShell):
 
         # Special case: for a one-arg filter any number of arguments are treated
         # as a single argument.
-        if (filter_name in self.tracer and 
+        if (filter_name in self.tracer and
             get_argcount_for_method(self.tracer[filter_name].__init__) == 1):
 
             filter_args = (' '.join(args), )
@@ -151,42 +151,42 @@ class Shell(HistorySavingDefaultShell):
             filter_args = args
         else:
             filter_args = None
-            
+
         def action():
             self.tracer.run( [(filter_name, filter_args)], self.files )
             print
 
         self.redirecting_stdout(action, filter_name, filter_args)
-        
+
     def do_runmany(self, args):
         # XXX: HACK HACK HACK HACK
         filters = args.split()
         if not args: return
-        
+
         def action():
             self.tracer.run( [( filter_name, () ) for filter_name in filters], self.files )
             print
-        
+
         self.redirecting_stdout(action, '|'.join(filters), ())
-            
+
     def do_backtrace(self, args):
         '''Displays the exception backtrace for the last failed filter.'''
         if self.last_exception:
             sys.excepthook(*self.last_exception)
     do_bt = do_backtrace
-    
+
     def create_pager_pipe(self):
         if not self.pager_path:
             raise RuntimeException('No pager was given.')
-        
+
         return subprocess.Popen(
             (self.pager_path, '-'),
             stdin=subprocess.PIPE,
             stdout=None, stderr=None,
             shell=False)
-            
+
     def do_into(self, args):
-        '''Sets or displays the destination for filter output. The special filename 
+        '''Sets or displays the destination for filter output. The special filename
 StdoutFilename will redirect filter output to the console.'''
         def print_output_destination():
             if self.output_file is None:
@@ -195,16 +195,16 @@ StdoutFilename will redirect filter output to the console.'''
                 msg("Filter output will be paged with %s.", self.pager_path)
             else:
                 msg("Filter output will be redirected to: %s", self.output_file)
-                
+
         args = args.split(' ', 1)
         output_file = args[0]
-        
+
         if output_file:
             if output_file == StdoutFilename:
                 self.output_file = None
             else: # we will treat the value PagerFilename specially
                 self.output_file = output_file
-                
+
         print_output_destination() # report on output destination in any case
 
     def redirecting_stdout(self, action, filter_name, filter_args):
@@ -212,41 +212,41 @@ StdoutFilename will redirect filter output to the console.'''
 a pager program.'''
         try:
             old_stdout = sys.stdout
-            
+
             pipe = None
-            
+
             if self.output_file == PagerFilename:
                 pipe = self.create_pager_pipe()
                 sys.stdout = pipe.stdin
             elif self.output_file:
                 sys.stdout = open(self.output_file, 'w')
-                
+
             action()
-            
-            if self.output_file == PagerFilename: 
+
+            if self.output_file == PagerFilename:
                 if pipe:
                     sys.stdout.close() # Signal EOF
                     pipe.wait()
-                
+
         except KeyboardInterrupt:
             if pipe:
                 pipe.stdin.close()
                 pipe.wait()
-                
+
             msg("\nFilter run %s halted by user.", filter_run_name(filter_name, filter_args))
-             
+
         except Exception, e:
             if pipe:
                 pipe.stdin.close()
                 pipe.wait()
-                
+
             msg("Filter run %s halted by framework:", filter_run_name(filter_name, filter_args))
             msg("\t%s (%s)", e.message, e.__class__.__name__)
-            
+
             self.last_exception = sys.exc_info()
         finally:
             sys.stdout = old_stdout
-            
+
             if pipe:
                 pipe.stdin.close()
 
@@ -256,18 +256,18 @@ a pager program.'''
                            dest='show_mode', action='store_const', const='pp_subtree'),
                make_option('-w', '--whole-tree', help='Print whole tree on match (not just matching subtrees).',
                            dest='show_mode', action='store_const', const='whole_tree'),
-               make_option('-W', '--pp-whole-tree', help='Pretty print whole tree on match (not just matching subtrees).',                         
+               make_option('-W', '--pp-whole-tree', help='Pretty print whole tree on match (not just matching subtrees).',
                            dest='show_mode', action='store_const', const='pp_whole_tree'),
-                           
+
                make_option('-m', '--matched-tag-only', help='Print matched tag only.',
                            dest='show_mode', action='store_const', const='matched_tag'),
-                           
+
                make_option('-b', '--tags-and-text', help='Print tree tags and text under.',
                            dest='show_mode', action='store_const', const='tags_and_text'),
-                           
+
                make_option('-T', '--tags', help='Print tree tags.',
                            dest='show_mode', action='store_const', const='tags'),
-                           
+
                make_option('-l', '--label', help='Print labels of matching trees.',
                            dest='show_mode', action='store_const', const='label'),
                make_option('-t', '--tokens', help='Print tokens of matching trees.',
@@ -276,7 +276,7 @@ a pager program.'''
                            dest='show_mode', action='store_const', const='tokens_only'),
                make_option('-r', '--rule', help='Shows the local category context of the match.',
                            dest='show_mode', action='store_const', const='rule'),
-                           
+
                make_option('-a', '--find-all', help='Find all matches (not just the first).',
                            dest='find_mode', action='store_const', const='all', default='all'),
                make_option('-1', '--find-first', help='Match only one node where possible.',
@@ -319,16 +319,16 @@ a pager program.'''
 
     def get_filter_attributes(self, attr):
         return [getattr(filter, attr) for filter in self.tracer.available_filters_dict.values()]
-            
+
     def get_long_forms(self):
         return ["--" + long_opt for long_opt in self.get_filter_attributes('long_opt')]
-        
+
     def get_short_forms(self):
         return ["-" + opt for opt in self.get_filter_attributes('opt')]
-        
+
     def complete_run(self, text, line, begin_index, end_index):
         '''Returns completions for the _run_ command: either a long option name, a short one, or a filter name.'''
-        # Be careful with the cmd module; it seems to swallow all getattr exceptions silently (eg accessing 
+        # Be careful with the cmd module; it seems to swallow all getattr exceptions silently (eg accessing
         # a non-existent member of this class succeeds)
         if text.startswith('--'):
             return [opt for opt in self.get_long_forms() if opt.startswith(text)]
@@ -338,36 +338,36 @@ a pager program.'''
             # Case insensitive comparison here
             filter_names = [name for name in self.tracer.available_filters_dict.keys() if name.lower().startswith(text.lower())]
             return filter_names
-            
+
     def complete_with(self, text, line, begin_index, end_index):
         '''Returns completions for the _with_ command: paths of which the given text is a prefix.'''
         # TODO: make optionally case-insensitive. Is there a better way short of giving a glob [Ll][Ii][Kk][Ee] this?
         return self.filename_complete(text)
-        
+
     def complete_load(self, text, line, begin_index, end_index):
         '''Returns completions for the _load_ command: all modules available to Python.'''
         return [module for module in sys.modules.keys() if module.startswith(text)]
-        
+
     def filename_complete(self, text):
         '''Returns a list of files and directories whose names are prefixes of the entered
 text. Appends a directory separator to directory completions.'''
-        return [path + (os.path.sep if os.path.isdir(path) else '') 
-                for path in glob.glob(text + '*') 
+        return [path + (os.path.sep if os.path.isdir(path) else '')
+                for path in glob.glob(text + '*')
                 if path.startswith(text)]
-                
+
 def run_file(option, opt_string, value, parser, *args, **kwargs):
     '''Reads and invokes commands from a file, then terminates.'''
     script_file = value
     if not os.path.exists(script_file):
         raise RuntimeError('Script file %s not found.' % script_file)
-        
+
     sh = Shell()
     for line in open(script_file):
         line = line.strip()
         sh.onecmd(line)
-        
+
     sys.exit()
-    
+
 def set_config_file(option, opt_string, value, parser, *args, **kwargs):
     try:
         config.config_file = value
@@ -375,7 +375,7 @@ def set_config_file(option, opt_string, value, parser, *args, **kwargs):
         err("Couldn't load config file `%s': %s", value, e)
 
 def register_builtin_switches(parser):
-    parser.add_option('-F', '--file-script', help='Reads and invokes cmd.py commands from a file, then terminates. Ignores input files given on the command line.', 
+    parser.add_option('-F', '--file-script', help='Reads and invokes cmd.py commands from a file, then terminates. Ignores input files given on the command line.',
                       type='string', nargs=1, action='callback', callback=run_file)
     parser.add_option('-c', '--config', help="Set config file.", type='string', nargs=1,
                       action='callback', callback=set_config_file)
@@ -390,22 +390,25 @@ def register_builtin_switches(parser):
                       action='store_false', dest='verbose')
     parser.add_option('-H', '--clear-history', help='Clear the history file.',
                       action='store_true', dest='clear_history')
-        
+
 from optparse import OptionParser
 if __name__ == '__main__':
     try:
         import psyco
         psyco.full()
     except ImportError: pass
-    
+
     parser = OptionParser(conflict_handler='resolve')
     parser.set_defaults(verbose=True, clear_history=False)
     register_builtin_switches(parser)
-    
+
     opts, remaining_args = parser.parse_args(sys.argv)
     argv = remaining_args[1:] # Take any remaining arguments as input files to start with
-    
+
     parser.destroy()
 
-    sh = Shell(pager_path=opts.pager_path, files=argv, verbose=opts.verbose, clear_history=opts.clear_history)
+    sh = Shell(pager_path=opts.pager_path,
+               files=argv,
+               verbose=opts.verbose,
+               clear_history=opts.clear_history)
     sh.cmdloop()
