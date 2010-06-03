a76f656d849653a99f5a6585c7a507e984f00c32
Daniel Tse <daniel@semanaut.com>
2010-06-03 04:49:42 +0000

    added comments to make_markedup
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@1253 d32a69a3-4c7f-4454-b852-5abc192257b5

--- make_markedup.sh
+++ make_markedup.sh
@@ -9,9 +9,13 @@ fi
 threshold=$1; shift
 corpus=$@
 
+# count all categories over the corpus
 egrep -ho '<L [^ ]+' $corpus | cut -c3- | sort | uniq -c | sort -rn | \
+    # filter out categories under the threshold
     gawk "(\$1 >= $threshold) { print \$2 }" | \
+    # generate markedup
     python -m'apps.cn.mkmarked' | \
+    # insert [X] feature variables
     perl -ne 's/S(?!\[)/S\[X\]/g if $_ =~ /^\s+/; print' | \
     # add dependency slots and 'ignore' entries
     perl -ne '$i=1; s/(?=\){_})/$v="<$i>";++$i;$v;/ge; s/^\t\d+/"\t".($i-1)/ge; print $_; if ($i>0) {print "\t$_ ignore\n" for 1..($i-1)}'
