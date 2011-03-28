9925b9c482c0af3471825c8e7e7db2de8ada385a
Daniel Tse <daniel@semanaut.com>
2011-03-28 04:58:48 +0000

    slightly changed new analysis from previous revision
    git-svn-id: http://svn.ask.it.usyd.edu.au/daniel/svn0/Doc/tools@1491 d32a69a3-4c7f-4454-b852-5abc192257b5

--- apps/cn/tag.py
+++ apps/cn/tag.py
@@ -324,7 +324,7 @@ def preprocess(root):
             #     /  \
             # NP-APP NP
             # we rewrite X's tag to be NP-APP to avoid the spurious rules like NP N -> N
-            expr = r'''/NP/ <1 { /NP/=P <1 /NP(-.+)?-APP/ } <2 /NP/'''
+            expr = r'''/NP/ <1 { /NP/=P <1 /NP(-.+)?-APP/ }'''
             result = get_first(node, expr, with_context=True)
             if result:
                 _, ctx = result
