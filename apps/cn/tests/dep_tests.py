01d1ba484bad3948a9ce2562660145976749f1c2
Daniel Tse <daniel@semanaut.com>
2011-06-07 05:35:07 +0000

    added dep_tests for passive analyses

--- apps/cn/tests/dep_tests.py
+++ apps/cn/tests/dep_tests.py
@@ -46,6 +46,7 @@ class DepTests(unittest.TestCase):
         self.check('apps/cn/tests/test2.ccg', 'apps/cn/tests/test2.gs')
         self.check('apps/cn/tests/test3.ccg', 'apps/cn/tests/test3.gs')
         self.check('apps/cn/tests/test4.ccg', 'apps/cn/tests/test4.gs')
+        self.check('apps/cn/tests/passives.ccg', 'apps/cn/tests/passives.gs')
 #        self.check('final/chtb_9992.fid', 'apps/cn/tests/blah.gs')
                     
 if __name__ == '__main__':
