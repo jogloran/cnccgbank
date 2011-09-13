c6fba72c1e4f39a581575fab21e8540a36275c00
Daniel Tse <daniel@semanaut.com>
2011-09-13 02:04:02 +0000

    fixed wrong formatting in ccg synttree output

--- munge/trees/synttree.py
+++ munge/trees/synttree.py
@@ -3,11 +3,37 @@ from munge.trees.traverse import text
 
 def process_lex_node_repr(node, compress=False):
     if compress:
+        return "\\Pos{%s} %s \\cjk{%s}" % (node.tag, "\\edge[roof]; " if node.count()>1 else '', ' '.join(text(node)))
+    if node.is_leaf():
+        return "{\\Pos{%s} \\cjk{%s}}" % (node.tag, node.lex)
+    else:
+        return "\\Pos{%s}" % node.tag
+        
+Lnode_id = 0
+def process_lex_node_reprL(node, compress=False):
+    if compress:
         return "%s %s \\cjk{%s}" % (node.tag, "\\edge[roof]; " if node.count()>1 else '', ' '.join(text(node)))
     if node.is_leaf():
-        return "%s \\cjk{%s}" % (node.tag, node.lex)
+        global Lnode_id
+        result = "%s \\node(l%s){\\cjk{%s}};" % (node.tag, Lnode_id, node.lex)
+        Lnode_id += 1
+        return result
     else:
         return "%s" % node.tag
         
+from apps.ccgdraw import sanitise_category
+Rnode_id = 0
+def process_lex_node_reprR(node, compress=False):
+    if compress:
+        return "\\cf{%s} %s \\cjk{%s}" % (sanitise_category(str(node.category)), "\\edge[roof]; " if node.count()>1 else '', ' '.join(text(node)))
+    if node.is_leaf():
+        global Rnode_id
+        result = "\\cf{%s} \\node(r%s){\\cjk{%s}};" % (sanitise_category(str(node.category)), Rnode_id, node.lex)
+        Rnode_id += 1
+        return result
+    else:
+        return "\\cf{%s}" % sanitise_category(str(node.category))
+        
 pprint_synttree = pprint_with(process_lex_node_repr, open='[.', close=' ]', bracket_outermost=False, do_reduce=False)
-pprint_synttree_top = pprint_with(process_lex_node_repr, open='[.', close=' ]', bracket_outermost=False, detail_level=1, do_reduce=False)
+pprint_synttreeL = pprint_with(process_lex_node_reprL, open='[.', close=' ]', bracket_outermost=False, do_reduce=False)
+pprint_synttreeR = pprint_with(process_lex_node_reprR, open='[.', close=' ]', bracket_outermost=False, do_reduce=False)
