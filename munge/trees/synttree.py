b5c54580123dcb709df00443726e1eadba8a0579
daniel.tse <daniel@semanaut.com>
2011-10-14 15:41:14 +1100

    fixes to synttree print
    because of Lnode_id and Rnode_id not being reset to 0, the
    leaf-to-leaf edges could be misaligned

--- munge/trees/synttree.py
+++ munge/trees/synttree.py
@@ -15,18 +15,24 @@ def process_lex_node_repr(node, compress=False):
         return "{\\Pos{%s} %s}" % (node.tag, latex_tag_for(node.lex))
     else:
         return "\\Pos{%s}" % node.tag
+        
+def is_trace(tag):
+    return tag == '-NONE-'
 
 Lnode_id = 0
 def process_lex_node_reprL(node, compress=False):
     if compress:
-        return "%s %s \\cjk{%s}" % (node.tag, "\\edge[roof]; " if node.count()>1 else '', ' '.join(text(node)))
+        return "\\Pos{%s} %s \\cjk{%s}" % (node.tag, "\\edge[roof]; " if node.count()>1 else '', ' '.join(text(node)))
     if node.is_leaf():
-        global Lnode_id
-        result = "%s \\node(l%s){%s};" % (node.tag, Lnode_id, latex_tag_for(node.lex))
-        Lnode_id += 1
+        if is_trace(node.tag):
+            result = "\\node{\\Pos{%s} %s};" % (node.tag, latex_tag_for(node.lex))
+        else:
+            global Lnode_id
+            result = "\\node(l%s){\\Pos{%s} %s};" % (Lnode_id, node.tag, latex_tag_for(node.lex))
+            Lnode_id += 1
         return result
     else:
-        return "%s" % node.tag
+        return "\\Pos{%s}" % node.tag
         
 Rnode_id = 0
 def process_lex_node_reprR(node, compress=False):
@@ -39,7 +45,12 @@ def process_lex_node_reprR(node, compress=False):
         return result
     else:
         return "\\cf{%s}" % sanitise_category(str(node.category))
-        
+
+def reset_ids():
+    global Lnode_id
+    global Rnode_id
+    Lnode_id = Rnode_id = 0
+    
 pprint_synttree = pprint_with(process_lex_node_repr, open='[.', close=' ]', bracket_outermost=False, do_reduce=False)
 pprint_synttreeL = pprint_with(process_lex_node_reprL, open='[.', close=' ]', bracket_outermost=False, do_reduce=False)
 pprint_synttreeR = pprint_with(process_lex_node_reprR, open='[.', close=' ]', bracket_outermost=False, do_reduce=False)
