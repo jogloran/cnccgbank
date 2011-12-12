068062122db7723e3b5b1691dfcc799a82f6cffc
daniel.tse <daniel@semanaut.com>
2011-12-12 16:32:31 +1100

    added script for avg #tags/word
    added some flexibility to Tabulation

--- apps/util/tabulation.py
+++ apps/util/tabulation.py
@@ -5,17 +5,21 @@ def decimal_length(n):
     '''Returns the length of _n_ in decimal digits. Undefined for n<0.'''
     return math.floor(math.log10(n) + 1)
 
-def Tabulation(table_var):
+def Tabulation(table_var, reducer=lambda e:e, value_maker=int, limit=None):
     class _Tabulation(object):
         def __init__(self):
-            setattr(self, table_var, defaultdict(int))
+            setattr(self, table_var, defaultdict(value_maker))
+            self.reducer = reducer
         
         def output(self):
             table = getattr(self, table_var)
-            max_freq_length = decimal_length( max(table.values()) )
+            max_freq_length = decimal_length(reducer(max(table.values(), key=reducer)))
+                
             template = "%% %sd | %%s" % (int(max_freq_length)+1)
             
-            for k, freq in sorted(table.iteritems(), key=lambda e: e[1], reverse=True):
+            # a range endpoint of None corresponds to omitting the endpoint
+            for k, freq in sorted(table.iteritems(), key=lambda e: reducer(e[1]), reverse=True)[:limit]:
+                freq = reducer(freq)
                 print template % (freq, k)
         
     return _Tabulation
