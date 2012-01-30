abe24c8ac3bbe4a3647bd7a628918f1d1f73d815
daniel.tse <daniel@semanaut.com>
2012-01-30 20:58:14 +1100

    refining expressions

--- apps/anno/nldfind.py
+++ apps/anno/nldfind.py
@@ -4,11 +4,13 @@ from munge.util.tgrep_utils import get_first
 
 class NLDFinder(Filter, OutputPrefacedPTBDerivation):
     Patterns = [
-        (r'/CP/ <1 { /WHNP/ < { "-NONE-" & ^/\*OP\*/ } } < { /CP/ < { /IP/ << { /NP-OBJ/ < "-NONE-" $ /V/=V } } < /DEC/ }', 'objex'),
-        (r'/CP/ <1 { /WHNP/ < { "-NONE-" & ^/\*OP\*/ } } < { /CP/ < { /IP/ << { /NP-SBJ/ < "-NONE-" $ /V/=V } } < /DEC/ }', 'subjex'),
-        (r'/CP/ <1 { /WHNP/ < { "-NONE-" & ^/\*OP\*/ } } < {          /IP/ << { /NP-OBJ/ < "-NONE-" $ /V/=V } }', 'objex_null'),
-        (r'/CP/ <1 { /WHNP/ < { "-NONE-" & ^/\*OP\*/ } } < {          /IP/ << { /NP-SBJ/ < "-NONE-" $ /V/=V } }', 'subjex_null'),
-        (r'/IP/ < /-TPC-\d+/a << { "-NONE-" & ^/\*T\*-\d+/ }', 'gaptop'),
+    # F = filler
+    # T = trace
+        (r'/CP/ <1 { /WHNP/ < { "-NONE-" & ^/\*OP\*/=T } } < { /CP/ < { /IP/ << { /NP-OBJ/ < "-NONE-" $ /V/=V } } < /DEC/ }', 'objex'),
+        (r'/CP/ <1 { /WHNP/ < { "-NONE-" & ^/\*OP\*/=T } } < { /CP/ < { /IP/ << { /NP-SBJ/ < "-NONE-" $ /V/=V } } < /DEC/ }', 'subjex'),
+        (r'/CP/ <1 { /WHNP/ < { "-NONE-" & ^/\*OP\*/=T } } < {          /IP/ << { /NP-OBJ/ < "-NONE-" $ /V/=V } }', 'objex_null'),
+        (r'/CP/ <1 { /WHNP/ < { "-NONE-" & ^/\*OP\*/=T } } < {          /IP/ << { /NP-SBJ/ < "-NONE-" $ /V/=V } }', 'subjex_null'),
+        (r'/IP/ < /-TPC-\d+/a=F << { * < { "-NONE-" & ^/\*T\*-\d+/=T } $ /V/=V }', 'gaptop'),
         (r'/LB/ $ { /IP/ << { "-NONE-" & ^/\*-\d+/ } }', 'lb_gap'),
         (r'/LB/ $ /IP/', 'lb_nongap'),
         (r'/SB/ $ { /IP/ << { "-NONE-" & ^/\*-\d+/ } }', 'sb_gap'),
