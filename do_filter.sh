675beeb91afbcf87a158c76209648a3cd5d744fb
Daniel Tse <daniel@semanaut.com>
2012-02-22 12:54:16 +1100

    trying to put corpus build products in the data directory

--- do_filter.sh
+++ do_filter.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+filtered_output_file=$1
+
 rm -rf filtered
 ./t -q -lapps.cn.badatom --bad-atom filtered final/*/*
-cat filtered/* > filtered_corpus
+cat filtered/* > "$filtered_output_file"
