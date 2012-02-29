2fe7d8e84326a55b0f5e8ad4e9b14749c906df2f
Daniel Tse <daniel@semanaut.com>
2012-02-29 12:25:39 +1100

    locate.py now accepts -e switch
    -e switch looks for derivations with contiguous tokens (not just 'tokens are all found in
    the given derivation')

--- locate.py
+++ locate.py
@@ -13,13 +13,33 @@ import sys
 import re
 import os
 
-sought = set(sys.argv[1:])
+if sys.argv[1] == '-e':
+    exact = True
+    sys.argv = sys.argv[1:]
+else:
+    exact = False
+
+if exact:
+    sought = sys.argv[1:]
+else:
+    sought = set(sys.argv[1:])
+
 corpus = 'corpora/cptb/segmented'
 
 XML, PLAIN = 1, 2
 def determine_format_from(line):
     if line.startswith('<DOC'): return XML
     else: return PLAIN
+
+def contains_sublist(lst, sublst):
+    n = len(sublst)
+    return any((sublst == lst[i:i+n]) for i in xrange(len(lst)-n+1))
+
+def matches(sought, candidate, exact=False):
+    if exact:
+        return contains_sublist(candidate, sought)
+    else:
+        return sought <= set(candidate)
 
 for path, dirs, files in os.walk(corpus):
     for fn in files:
@@ -41,16 +61,17 @@ for path, dirs, files in os.walk(corpus):
                 if format == XML:
                     if line.startswith('<S ID'):
                         doc_sent_no += 1
-                        candidate = set(lines.next().rstrip().split())
 
-                        if sought <= candidate:
+                        candidate = lines.next().rstrip().split()
+                        if matches(sought, candidate, exact=exact):
                             print fn, doc_sent_no
+                            print ' '.join(candidate)
                 else:
                     doc_sent_no += 1
-                    candidate = set(line.rstrip().split())
+                    candidate = line.rstrip().split()
 
-                    if sought <= candidate:
+                    if matches(sought, candidate, exact=exact):
                         print fn, doc_sent_no
-
+                        print ' '.join(candidate)
 
         except StopIteration: pass
