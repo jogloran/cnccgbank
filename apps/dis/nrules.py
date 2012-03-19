30a88e83c9892bca2fa9065c8e2d0109ad7f63da
daniel.tse <daniel@semanaut.com>
2012-03-19 15:43:23 +1100

    added NRules script to find number of ptb rules
    changed Tabulation to support multiple dicts

---
+++ apps/dis/nrules.py
@@ -0,0 +1,47 @@
+# Chinese CCGbank conversion
+# ==========================
+# (c) 2008-2012 Daniel Tse <cncandc@gmail.com>
+# University of Sydney
+
+# Use of this software is governed by the attached "Chinese CCGbank converter Licence Agreement"
+# supplied in the Chinese CCGbank conversion distribution. If the LICENCE file is missing, please
+# notify the maintainer Daniel Tse <cncandc@gmail.com>.
+
+# Number of rules in corpus
+
+from apps.cn.fix_utils import base_tag
+from apps.util.tabulation import Tabulation
+from munge.trees.traverse import nodes
+from munge.proc.filter import Filter
+from itertools import ifilter
+
+class NRules(Tabulation(['freqs', 'unary']), Filter):
+    def __init__(self):
+        super(NRules, self).__init__()
+        
+    @staticmethod
+    def signature(node):
+        return ' '.join([base_tag(node.tag)] + [base_tag(kid.tag) for kid in node])
+        
+    def accept_derivation(self, bundle):
+        for node in nodes(bundle.derivation):
+            if node.is_leaf(): continue
+            if node.count() == 1 and node[0].is_leaf():
+                self.unary[ self.signature(node) ] += 1
+            
+            self.freqs[ self.signature(node) ] += 1
+        
+    def output(self):
+        ncats = len(self.freqs)
+        ncatsge10 = len(list(ifilter(lambda (k,v): v>=10, self.freqs.iteritems())))
+        print "#rules     : %d" % ncats
+        print "#rules >=10: %d" % ncatsge10
+        print
+        
+        ncats_nounary = len(list(ifilter(lambda (k,v): k not in self.unary, self.freqs.iteritems())))
+        ncatsge10_nounary = len(list(ifilter(lambda (k,v): v>= 10 and k not in self.unary, self.freqs.iteritems())))
+        print "#rules (without unary)     : %d" % ncats_nounary
+        print "#rules (without unary) >=10: %d" % ncatsge10_nounary
+        print
+        
+        self.do_output()
