b505949e91d01632c05ac0139ddbbe8cdb36a789
Daniel Tse <daniel@semanaut.com>
2012-06-20 22:15:13 +1000

    keeping track of ncats>=10

--- apps/dis/ncats.py
+++ apps/dis/ncats.py
@@ -10,6 +10,7 @@
 from apps.util.tabulation import Tabulation
 from munge.proc.filter import Filter
 from itertools import ifilter
+from munge.util.dict_utils import sorted_by_value_desc
 
 class NCats(Tabulation('freqs'), Filter):
     def __init__(self):
@@ -25,11 +26,14 @@ class NCats(Tabulation('freqs'), Filter):
         print "#cats >=10: %d" % ncatsge10
         
 class IncrementalNCats(Tabulation('freqs'), Filter):
-    def __init__(self):
+    def __init__(self, growth_fn, rank_fn):
         super(IncrementalNCats, self).__init__()
         
         self.ntokens = 0
-        self.data_points = [ (0, 0) ]
+        self.data_points = [ (0, 0, 0) ]
+
+        self.growth_fn = growth_fn
+        self.rank_fn = rank_fn
         
     def accept_leaf(self, leaf):
         self.freqs[str(leaf.cat)] += 1
@@ -38,7 +42,15 @@ class IncrementalNCats(Tabulation('freqs'), Filter):
         self.ntokens += len(bundle.derivation.text())
         
         ncats = len(self.freqs)
-        self.data_points.append( (self.ntokens, ncats) )
+        ncatsge5 = len(list(ifilter(lambda (k,v): v>=5, self.freqs.iteritems())))
+        self.data_points.append( (self.ntokens, ncats, ncatsge5) )
         
     def output(self):
-        print '\n'.join( ' '.join(map(str, xy)) for xy in self.data_points )
+        with file(self.growth_fn, 'w') as f:
+            print >>f, '\n'.join( ' '.join(map(str, xy)) for xy in self.data_points )
+
+        with file(self.rank_fn, 'w') as f:
+            for k, freq in sorted_by_value_desc(self.freqs):
+                print >>f, freq
+
+    arg_names = "GROWTH_FILENAME RANK_FILENAME"
