32325c64d211910522646c2a0b452def9944a9ca
Daniel Tse <daniel@semanaut.com>
2012-06-25 16:16:37 +1000

    added legends to growth/rank plots

--- scripts/make_growth_rank_plots.py
+++ scripts/make_growth_rank_plots.py
@@ -1,23 +1,29 @@
+from matplotlib import rc
+rc('font',**{'family':'serif','serif':['Times']})
+rc('text', usetex=True)
+   
 from pylab import *
 
-def make_data(base):
+def make_data(base, yname):
     clf()
     growth = transpose(genfromtxt(base + '.growth'))
-    xlabel('Number of tokens')
-    ylabel('Number of categories')
+    xlabel(r'\textit{Number of tokens}')
+    ylabel(r'\textit{Number of ' + yname + '}')
     plot(growth[0], growth[1])
     if size(growth, 0) > 2:
-        plot(growth[0], growth[2])
-    savefig(base + '.growth.png')
+        plot(growth[0], growth[2], '--')
+        legend([ 'All', '$f \ge 5$' ], 'best')
+    savefig(base + '.growth.pdf')
     
     clf()
     rank = transpose(genfromtxt(base + '.rank'))
-    xlabel('Rank order (log)')
-    ylabel('Frequency (log)')
+    xlabel(r'\textit{Rank order} ($\log$)')
+    ylabel(r'\textit{Frequency} ($\log$)')
+
     plot(rank, 'o')
     loglog()   
-    savefig(base + '.rank.png')
+    savefig(base + '.rank.pdf')
     
 if __name__ == '__main__':
-    make_data('cats')
-    make_data('rules')
+    make_data('cats', yname='categories')
+    make_data('rules', yname='rule types')
