11b6d1dcf6ea669f593a2e94c99b4ecf50d23add
Daniel Tse <daniel@semanaut.com>
2012-06-25 17:09:28 +1000

    added script to convert mkdeps_errors to a cmd script

---
+++ scripts/mkdeps_errors_to_cmd_script.py
@@ -0,0 +1,18 @@
+import sys, re
+
+seen_deriv = None
+
+print 'load apps.rulechk'
+print 'into stdout'
+
+for line in sys.stdin:
+    line = line.rstrip()
+
+    matches = re.search(r'Processing failed on derivation (\d+):(\d+)\((\d+)\)', line)
+    if matches:
+        seen_deriv = tuple(map(int, matches.groups()))
+
+    matches = re.findall(r'`(\d+)', line)
+    if matches:
+        print 'with fixed_np/%d,%d,%d' % seen_deriv
+        print '\n'.join( 'run CheckRules %s' % match for match in set(matches) )
