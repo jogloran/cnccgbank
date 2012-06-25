38bc957e5384ef37a69ce79efa82af0d5eea0fe3
Daniel Tse <daniel@semanaut.com>
2012-06-25 18:21:07 +1000

    sorted_by_value_desc now uses operator.itemgetter

--- munge/util/dict_utils.py
+++ munge/util/dict_utils.py
@@ -6,6 +6,7 @@
 # Use of this software is governed by the attached "Chinese CCGbank converter Licence Agreement"
 # supplied in the Chinese CCGbank conversion distribution. If the LICENCE file is missing, please
 # notify the maintainer Daniel Tse <cncandc@gmail.com>.
+import operator
 
 class CountDict(dict):
     '''Accessing a non-existent key in this dictionary will return the value zero.'''
@@ -20,7 +21,7 @@ class PrefixSet(set):
 
 def sorted_by_value_desc(dict):
     '''Given a _dict_, returns its (key, value) pairs sorted in descending order.'''
-    return sorted(dict.iteritems(), key=lambda (k, v): v, reverse=True)
+    return sorted(dict.iteritems(), key=operator.itemgetter(1), reverse=True)
 
 def smash_key_case(d):
     '''Given a dictionary _d_, lowercases its keys.'''
