46f48046d417770682b0322477a15dcc7300cc72
Daniel Tse <daniel@semanaut.com>
2013-03-06 11:46:09 +1100

    fixed corpusparts.py

--- corpusparts.py
+++ corpusparts.py
@@ -39,10 +39,9 @@ for fn in sys.argv[2:]:
 print """
 set terminal epslatex input color size 19cm, 4cm
 set output 'corpusparts.tex'
-set terminal x11 persist
 set xrange [ -0.500000 : %(length)d ] noreverse nowriteback
 set yrange [ 0.500000 : 1.5000 ] noreverse nowriteback
-set palette defined ( 0 'green', 1 'green', 1 'yellow', 2 'yellow', 2 'red', 3 'red', 3 'purple', 4 'purple', 4 'brown', 5 'brown')
+set palette defined ( 0 0.09765625 0.703125 0.56640625, 1 0.09765625 0.703125 0.56640625, 1 0.93359375 0.734375 0.0703125, 2 0.93359375 0.734375 0.0703125, 2 0.17578125 0.55078125 0.8359375, 3 0.17578125 0.55078125 0.8359375, 3 0.890625 0.2578125 0.20703125, 4 0.890625 0.2578125 0.20703125, 4 'white', 4 'white')
 set cbtics ("" 0, "Xinhua" 1, "HKSAR" 2, "Sinorama" 3, "Broadcast" 4) scale 0
 set cbrange [ 1 : %(max_value)d ] noreverse nowriteback
 set xlabel "Number of words"
